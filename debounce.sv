44764f9d41bdb8b8558fe4003bbed0613e022423
CheeJiaXu <114555103+CheeJiaXu@users.noreply.github.com>
2023-09-07 16:18:44 +1000

    Update debounce.sv

--- debounce.sv
+++ debounce.sv
@@ -1,40 +1,30 @@
-// module debounce (
-//     input clk, button,
-//     output reg button_pressed
-// );
-//   localparam delay_val = 2500; // 50us with clk period 20ns is ____ counts
-//   int count = 0;
-//   reg deb = 0;
-  
-//   // Your code here!
-//   always @(posedge clk) begin
-//     if (button != deb) begin
-//       deb <= button;
-//       count <= 0;
-//     end
-//     else if (count == delay_val) 
-//       button_pressed <= deb;
-//     else
-//       count <= count + 1;
-//   end 
-
-// endmodule
-
 module debounce (
     input clk, button,
-    output reg button_pressed
+    output reg button_edge
 );
-  parameter delay_val = 2; 
-  integer count = 0;
-  reg new_button_pressed=0;
+  localparam delay_val = 2500; // 50us with clk period 20ns is ____ counts
+  int count = 0;
+  reg deb = 0;
+  
+  // Your code here!
   always @(posedge clk) begin
-      if (button != new_button_pressed) begin
-        new_button_pressed <= button;
-        count <= 0;
-      end
-      else if (count == delay_val) button_pressed <= new_button_pressed;
-      else count <= count + 1;
+    if (button != deb) begin
+      deb <= button;
+      count <= 0;
+    end
+    else if (count == delay_val) 
+      button_pressed <= deb;
+    else
+      count <= count + 1;
+  end 
+
+  logic button_q0, button_edge;
+  
+  always_ff @(posedge clk) begin : edge_detect
+      button_q0 <= button_pressed;
   end
+  assign button_edge = (button_pressed > button_q0);
+
 endmodule
 
 
