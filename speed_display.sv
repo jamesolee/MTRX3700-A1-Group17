23ab6d5a3745a961057c783baf11d101950fec37
CheeJiaXu <114555103+CheeJiaXu@users.noreply.github.com>
2023-09-08 19:15:34 +1000

    reset not working

--- speed_display.sv
+++ speed_display.sv
@@ -1,7 +1,7 @@
 module speed_display(
     input enable,
     input read_enable,
-	input reset,
+	 input reset,
     input      [1:0]  instruction,
     input      [2:0]  torque,
     output logic [6:0]  HEX4, // right digit
@@ -34,7 +34,7 @@ module speed_display(
                         (torque == 2)? 4:
                         (torque == 3)? 6: 
                         (torque == 4)? 8:0;   
-                acc_br = acc_fl;
+                acc_br = acc_bl;
                 acc_fl = 0;
                 acc_fr = 0;
             end
@@ -76,28 +76,33 @@ module speed_display(
     end
 
     logic [3:0]bcd_r1=12, bcd_r2=12, bcd_l1 = 12, bcd_l2 = 12;
+	 logic [3:0]n_bcd_r1=12, n_bcd_r2=12, n_bcd_l1 = 12, n_bcd_l2 = 12;
+	 logic [3:0] next_vel_r, next_vel_l;
 
     always_ff @(posedge read_enable) begin
         //calculate current speed
-        vel_r <= (reset)? 0: 
-					 (vel_r-acc_br > 9)? vel_r: 
+        next_vel_r <=(vel_r-acc_br > 9)? vel_r: 
                      (vel_r >= acc_br)? vel_r+acc_fr-acc_br: 0; 
-        vel_l <= (reset)? 0:
-					 (vel_l-acc_bl > 9)? vel_l: 
+        next_vel_l <=(vel_l-acc_bl > 9)? vel_l: 
                      (vel_l >= acc_bl)? vel_l+acc_fl-acc_bl: 0; 
-    end
-
-    always_ff @(negedge read_enable) begin
-        bcd_r2 <= (~enable)? 12: //force HEX to be off
+		  n_bcd_r2 <= (~enable)? 12: //force HEX to be off
                  (vel_r > 9)? 9: 0;
-        bcd_r1 <= (~enable)? 12: //force HEX to be off
+        n_bcd_r1 <= (~enable)? 12: //force HEX to be off
                  (vel_r >= 9)? 9: vel_r;   
 
-        bcd_l2 <= (~enable)? 12: //force HEX to be off
+        n_bcd_l2 <= (~enable)? 12: //force HEX to be off
                  (vel_l > 9)? 9: 0;
-        bcd_l1 <= (~enable)? 12: //force HEX to be off
-                 (vel_l >= 9)? 9: vel_l;    
+        n_bcd_l1 <= (~enable)? 12: //force HEX to be off
+                 (vel_l >= 9)? 9: vel_l;
     end
+	 
+	 assign vel_r = reset? 0: next_vel_r;
+	 assign vel_l = reset? 0: next_vel_l;
+	 assign bcd_r2 = reset? 12: n_bcd_r2;
+	 assign bcd_r1 = reset? 12: n_bcd_r1;
+	 assign bcd_l2 = reset? 12: n_bcd_l2;
+	 assign bcd_l1 = reset? 12: n_bcd_l1;
+
 
     // logic [3:0]bcd_r2 = 12;
     // logic [3:0]test;
