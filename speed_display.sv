220672af44667fd5f5817aed59a3e2f539325088
CheeJiaXu <114555103+CheeJiaXu@users.noreply.github.com>
2023-09-08 18:42:07 +1000

    update speed_display

--- speed_display.sv
+++ speed_display.sv
@@ -1,19 +1,19 @@
 module speed_display(
     input enable,
     input read_enable,
-	 input reset,
+	input reset,
     input      [1:0]  instruction,
-    input      [1:0]  torque,
+    input      [2:0]  torque,
     output logic [6:0]  HEX4, // right digit
     output logic [6:0]  HEX5, // right tenth
     output logic [6:0]  HEX6, // left digit
     output logic [6:0]  HEX7  // left tenth
 );
 
-    logic [4:0]vel_r = 0;
-    logic [4:0]vel_l = 0;
-    logic [4:0]acc_fr, acc_br =0; //change in right speed
-    logic [4:0]acc_fl, acc_bl =0; //change in left speed
+    logic [3:0]vel_r;
+    logic [3:0]vel_l;
+    logic [3:0]acc_fr, acc_br; //change in right speed
+    logic [3:0]acc_fl, acc_bl; //change in left speed
 
     //set absolute change in speed for left & right wheel
     always_comb begin
@@ -75,27 +75,39 @@ module speed_display(
         endcase 
     end
 
-    logic [4:0]bcd_r1, bcd_r2, bcd_l1, bcd_l2;
+    logic [3:0]bcd_r1=12, bcd_r2=12, bcd_l1 = 12, bcd_l2 = 12;
 
     always_ff @(posedge read_enable) begin
         //calculate current speed
-        vel_r = (reset)? 0: 
-					 (vel_r-acc_br > 9)? vel_r: (vel_r+acc_fr-acc_br); 
-        vel_l = (reset)? 0:
-					 (vel_l-acc_bl > 9)? vel_l: (vel_l+acc_fl-acc_bl);
+        vel_r <= (reset)? 0: 
+					 (vel_r-acc_br > 9)? vel_r: 
+                     (vel_r >= acc_br)? vel_r+acc_fr-acc_br: 0; 
+        vel_l <= (reset)? 0:
+					 (vel_l-acc_bl > 9)? vel_l: 
+                     (vel_l >= acc_bl)? vel_l+acc_fl-acc_bl: 0; 
+    end
 
-        bcd_r2 = (~enable)? 12: //force HEX to be off
+    always_ff @(negedge read_enable) begin
+        bcd_r2 <= (~enable)? 12: //force HEX to be off
                  (vel_r > 9)? 9: 0;
-        bcd_r1 = (~enable)? 12: //force HEX to be off
+        bcd_r1 <= (~enable)? 12: //force HEX to be off
                  (vel_r >= 9)? 9: vel_r;   
 
-        bcd_l2 = (~enable)? 12: //force HEX to be off
+        bcd_l2 <= (~enable)? 12: //force HEX to be off
                  (vel_l > 9)? 9: 0;
-        bcd_l1 = (~enable)? 12: //force HEX to be off
-                 (vel_l >= 9)? 9: vel_l;     
+        bcd_l1 <= (~enable)? 12: //force HEX to be off
+                 (vel_l >= 9)? 9: vel_l;    
     end
+
+    // logic [3:0]bcd_r2 = 12;
+    // logic [3:0]test;
+
+    // always_ff @(posedge read_enable) begin
+    //     test <= (instruction > 0)? torque+test: 0;
+    //     bcd_r2 <= (~enable)? 12: test;
+    // end
     
-        //// Seven-Segment Displays
+    //// Seven-Segment Displays
     seven_seg u0(
     .bcd(bcd_r2),
     .segments(HEX4));
